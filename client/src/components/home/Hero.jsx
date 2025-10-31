e9b28abaf9d0d410e5d7e83155b5d422870bd457
Prince Bajgain <princebajagainprof@gmail.com>
2025-10-31 10:06:13 +0545

    Made a new shuttle change

--- client/src/components/home/Hero.jsx
+++ client/src/components/home/Hero.jsx
@@ -177,14 +177,6 @@ const Hero = () => {
 
                         </Link>
 
-                        <button className="flex items-center gap-2 border border-slate-400 hover:bg-green-50 transition rounded-full px-7 h-12 text-slate-700">
-
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-video size-5" aria-hidden="true"><path d="m16 13 5.223 3.482a.5.5 0 0 0 .777-.416V7.87a.5.5 0 0 0-.752-.432L16 10.5"></path><rect x="2" y="6" width="14" height="12" rx="2"></rect></svg>
-
-                            <span>Try demo</span>
-
-                        </button>
-
                     </div>
 
 
